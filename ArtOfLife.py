52654a8bb6227318bc5023b5db5beb801b17eaeb
Paul-Even <paul.even@edu.devinci.com>
2024-01-25 16:57:36 +0100

    deploy

--- ArtOfLife.py
+++ ArtOfLife.py
@@ -1,140 +1,143 @@
-# This is an example of a script to measure the heart rate of an user
-
-# Once you have put the info of your sensor in the resolve_stream function, it will find the stream and create an stream to access the 
-# data
-
 from pylsl import StreamInlet,resolve_stream #import pylsl to use LSL and recover the data from the sensors
-import numpy as np #import numpy to create the ecg_peak filter
-import matplotlib.pyplot as plt #import matplotlib to plot the graph
-import time
-import random
-import mido
+import numpy as np #import numpy to calculate the average of the EDA values
+import mido #import mido to send notes to loopMidi
 from mido import Message
+import time #import time to make the pulsing effect
 
-ports = mido.get_output_names()
-portname = [name for name in ports if 'Disklavier' in name]
+ports = mido.get_output_names() #getting the names of all the detected mido ports 
+print(ports)
+outport = mido.open_output('loopMIDI Port FL 1') #selecting the port to receive the "heartbeat" notes
+outport2 = mido.open_output('loopMIDI Port FL2 2') #selecting the port to receive the "breathing" notes
+outport3 = mido.open_output('loopMIDI Port MM 3')
 
-
-
-streams = resolve_stream('name','OpenSignals') #recover the list of LSL stream of type 'ecg'
-
+streams = resolve_stream('name','OpenSignals') #receive the LSL stream from OpenSignals
 stream = StreamInlet(streams[0]) #create an stream from the first stream of the list
 
-v = np.linspace(0.5 * np.pi, 1.5 * np.pi, 15) #create a linear vector between pi/2 and 3pi/2
-peak_filter = np.sin(v) #create a sin wave on the linear space
+in_resp = False #boolean checking if the user is inhaling or exhaling
+eda_value = 0 #mean value of all the EDA values received in 10 seconds
+pulse_count = 0 #counting the number of pulses
+n=0 #counting the number of values received
+inPulse = False #boolean checking if a pulse is happening
+keynote = 0 #defining the note to be sent as the "breathing" note
+r=0 #RGB values for the visuals' color
+g=0
+b=0
 
-n=0
+def pulse_effect(min, max, sleep): #function that creates the "pulse" effect on MadMapper
+    n = 20
+    count=0
+    while n < 100:
+        outport3.send(Message('note_on', note=127, velocity=n))
+        time.sleep(sleep)
+        n+=1
+        count+=1
+    #outport3.send(Message('note_on', note=126, velocity=n))
+    while n>20:
+        outport3.send(Message('note_on', note=127, velocity=n))
+        time.sleep(sleep)
+        n-=1
+        count+=1
+    #outport3.send(Message('note_on', note=126, velocity=n))
+    print(count)
 
 
-
-in_resp = False
-resp_count_final = 30
-position = 0
-eda_value = 0
-heart_rate = 0
 
 
 while True:
-
-    print("start")
-    data_eda = [] #create an empty list to recover the data
-    data_ecg = [] #create an empty list to recover the data
-    timestamp_ecg = [] #create an empty list to recover the timestamp_ecg
-    resp_count = 0
-    data_resp = [] #create an empty list to recover the data
+        n+=1
+        data_eda = [] #create an empty list to recover the eda data
+        timestamp = [] #create an empty list to recover the timestamp
+        resp_count = 0
     
-    while n < 10000: #collect 1000 datapoint
-        values, timestamp = stream.pull_sample() #recover the data and their timestamp_ecg
-        data_ecg.append(values[1]) #add the values to the data
-        timestamp_ecg.append(timestamp) #add the timestamp_ecg to the time data
-        if values[1] < -0.3:
-            last_index = position
-            position = n
-            #np.where(ecg_transform = i)
-            if position-last_index > 200:
-                #peak_count+=1
-                print(n)
+    
+        values, timestamp = stream.pull_sample() #recover the data and their timestamp
+        if values[1] < -0.4 and inPulse==False: #if the pulse value is under -0.4, we consider that a pulse is happening
                 print("heartbeat")
-                if(eda_value != 0):
-                    print("note de piano")
-                    with mido.open_output(portname[0]) as port:
-                        try:
-                            midi_note = max(0, min(resp_count_final*2, 128))
-                            print("midi = ", midi_note)
-                            port.send(Message('note_on', note=int(midi_note), velocity=eda_value*5))
-                            
-                            # Send a note-off message (optional)
-                            port.send(Message('note_off', note=int(midi_note), velocity=0))
-                        except ValueError:
-                            print("Invalid input. Please enter a valid integer between 21 and 108.")
+                inPulse = True
+                try:
+                        pulse_count +=1
+                        
+                        if(pulse_count%4 == 0): #output a note every 4 pulse
+                                
+                                outport.send(Message('note_on', note=60, velocity=127))
+                                pulse_effect(10,127,0.005)
+                                print("pulse sent")
+                except ValueError:
+                        print("Invalid input. Please enter a valid integer between 21 and 108.")
 
+        elif values[1] > 0 and inPulse==True:
+            inPulse = False
+            print("pulse false")
 
-        sample_eda, timestamp_eda = stream.pull_sample() #recover the sample_eda and the corresponding timestamp_eda from the stream
-        data_eda.append(values[2]) #add the sample_eda to the data
+        data_eda.append(values[2]) #add the latest eda value to an array
 
-        data_resp.append(values[3]) #add the sample to the data
+        if(n >= 10000): #calculate the average eda value every 10 seconds and resets the counter
+              eda_value = np.mean(data_eda)
+              print(eda_value)
+              outport3.send(Message('note_on', note=126, velocity=r))
+              outport3.send(Message('note_on', note=125, velocity=g))
+              outport3.send(Message('note_on', note=124, velocity=b))
+              
+              n=0
+
+        if(eda_value > 0): #choosing the note to be sent depending on the average eda value of the last 10s
+                
+                if(eda_value < 2):
+                    keynote = 30
+                    r=127
+                    g=127
+                    b=127
+                elif(eda_value > 2 and eda_value < 6):
+                      keynote = 35
+                      r=127
+                      g=127
+                      b=0
+                elif(eda_value > 6 and eda_value < 10):
+                      keynote = 40
+                      r=0
+                      g=127
+                      b=0
+                elif(eda_value > 10 and eda_value < 14):
+                      keynote = 45
+                      r=0
+                      g=127
+                      b=127
+                elif(eda_value > 14 and eda_value < 18):
+                      keynote = 50
+                      r=0
+                      g=0
+                      b=127
+                elif(eda_value > 18 and eda_value < 22):
+                      keynote = 55
+                      r=127
+                      g=0
+                      b=127
+                elif(eda_value > 22 and eda_value < 26):
+                      r=127
+                      g=0
+                      b=0
+                elif(eda_value > 26):
+                      keynote = 70
+                      r=63
+                      g=63
+                      b=63
+        
         if in_resp == True:
-            if values[3] < -0.1:
-                #print("fin resp")
+            if values[3] < -0.1: #detecting if the user is exhaling
+                print("fin resp")
                 in_resp = False
                 #print(resp_count)
                 #time.sleep(0.1)
+                outport2.send(Message('note_off', note=keynote)) #stopping the "breathing" note
+                outport3.send(Message('note_on', note=123, velocity=10)) #sending the "breathing" note
         elif in_resp == False:
-            if values[3] > 0.1:
-                 #print("début resp")
+            if values[3] > 0.1: #detecting if the user is inhaling
+                 print("début resp")
                  resp_count+=1
                  in_resp = True
                  #print(resp_count)
                  #time.sleep(0.1)
-
-        n+=1
+                 outport2.send(Message('note_on', note=keynote, velocity=127)) #sending the "breathing" note
+                 outport3.send(Message('note_on', note=123, velocity=50)) #sending the "breathing" note
 
         
-
-
-    print("end")
-
-    ecg_transform = np.correlate(data_ecg,peak_filter,mode='same') #correlate the ecg_peak filter with the signal to highlight the ecg_peak
-    print("length: ",len(ecg_transform))
-
-    ecg_peak = float(abs(max(ecg_transform))) * 0.50 #create the threshold for the max ecg_peak
-    print("max: ",ecg_peak)
-    time = timestamp_ecg[-1] - timestamp_ecg[0] #compute the total time of the data
-    peak_count = 0 #create a counter for the ecg_peak in the data
-    last_index = 0
-
-    for i in ecg_transform: #look at the number of ecg_peak in the data
-        if i > ecg_peak:
-            position = int(np.where(ecg_transform == i)[0][0])
-            #np.where(ecg_transform = i)
-            if position != last_index +1 and position != last_index +2:
-                peak_count+=1
-                #print(i)
-                
-                #print("index: ", np.where(ecg_transform == i)[0][0])
-            last_index = int(np.where(ecg_transform == i)[0][0])
-            #print("index: ", last_index)
-
-    peak_count+=0
-    heart_rate = (peak_count/time)*60 #to compute the heart rate divide the number of ecg_peak by the time and multiply it by 60 to have the bpm
-    resp_count = (resp_count/time)*60
-    #print("number of ecg_peak: ",peak_count)
-    #print("time: ",time)
-    eda_value = sum(data_eda)/len(data_eda)
-    print("heart rate: ",int(heart_rate))
-    print("EDA : ", eda_value)
-    print("Resp count: ",int(resp_count))
-    
-    print("oui")
-    list_value = []
-    n=0
-    resp_count_final = resp_count
-
-    #plt.plot(ecg_transform)
-    #plt.show()
-
-    
-
-    
-    
-    
